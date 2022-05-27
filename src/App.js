f23795d6611d998bb89ee0313db794a3b420bf68
nate-orage <nate54o@icloud.com>
2022-05-26 22:53:06 -0700

    dockerfile change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello. I'm Nate. Feature branch 2.
+          Hello. I'm Nate. Feature branch 3.
         </p>
         <a
           className="App-link"
