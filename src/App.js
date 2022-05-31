3327871193853170579dbf1c4cb74e505cdeedfe
nate-orage <nate54o@icloud.com>
2022-05-30 20:52:14 -0700

    feature test 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello. I'm Nate. Feature branch 3.
+          Hello. I'm Nate. Feature branch 4.
         </p>
         <a
           className="App-link"
